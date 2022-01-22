5370cc6fc828a1d2f169f096f63dac03a903bd1d
Calvin <83982075+calvinjujuis@users.noreply.github.com>
2022-01-21 22:48:58 -0800

    Add files via upload

--- biggie-calculator/biggie.c
+++ biggie-calculator/biggie.c
@@ -4,10 +4,18 @@
 #include <assert.h>
 #include "biggie.h"
 
+/////////////////////////////////////////////////////////////////////////////
+// DO NOT CHANGE THIS STRUCTURE
+/////////////////////////////////////////////////////////////////////////////
 struct biggie {
   bool negative;
   char *digits;
 };
+// notes: digits is a properly null-terminated dynamic string
+//        digits contains the number reversed, so 1203 => "3021"
+//        digits cannot have leading zeros, so 1203 cannot be "30210"
+//        zero is stored as "0" and cannot be negative (negative is false)
+/////////////////////////////////////////////////////////////////////////////
 
 struct biggie *biggie_create(const char *s) {
   assert(s);
@@ -60,6 +68,7 @@ void biggie_destroy(struct biggie *big) {
 }
 
 void biggie_print(const struct biggie *big, bool newline) {
+  assert(big);
   if (big->negative) {
     printf("-");
   }
@@ -72,7 +81,9 @@ void biggie_print(const struct biggie *big, bool newline) {
   }      
 }
 
-// effects: may modify digits
+// delete_leading_zero(digits) deletes leading zeros in *digits
+// effects: may modify *digits
+// time: O(n) where n is the length of digits ( strlen(digits) )
 static char *delete_leading_zero(char *digits) {
   int len = strlen(digits);
   int zero_count = 0;
@@ -89,6 +100,10 @@ static char *delete_leading_zero(char *digits) {
   return digits;
 }
 
+// plus_or_minus(n, m, operation, compare) returns the product of n
+//   and m depending on operation.
+// effects: may modify n
+// time: O(log(n)) where n is the max of strlen(n->digits) and strlen(m->digits)
 static void plus_or_minus(struct biggie *n, const struct biggie *m,
                           char operation, int compare) {
   int n_len = strlen(n->digits);
@@ -170,7 +185,8 @@ static void plus_or_minus(struct biggie *n, const struct biggie *m,
   }
 }
 
-// returns 1 if n > m and returns -1 otherwise
+// compare(n, m) returns 1 if n->digits > m->digits and returns -1 otherwise
+// time: O(n) where is the min of strlen(n->digits) and strlen(m->digits)
 static int compare(const char *n, const char *m) {
   int n_len = strlen(n);
   int m_len = strlen(m);
@@ -193,6 +209,8 @@ static int compare(const char *n, const char *m) {
 }
 
 void biggie_add(struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
   int cmp = compare(n->digits, m->digits);
   if (n->negative == m->negative) {
     plus_or_minus(n, m, '+', cmp);
@@ -212,6 +230,8 @@ void biggie_add(struct biggie *n, const struct biggie *m) {
 }
 
 void biggie_sub(struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
   int cmp = compare(n->digits, m->digits);
   if (n->negative != m->negative) {
     plus_or_minus(n, m, '+', cmp);
@@ -231,20 +251,16 @@ void biggie_sub(struct biggie *n, const struct biggie *m) {
 }
 
 void biggie_mult(struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
   int m_len = strlen(m->digits);
-  int NEGATIVE = 0;
-  if (n->negative == m->negative) {
-    // positive if same sign
-    NEGATIVE = 0;
-  } else {
-    NEGATIVE = 1;
-  }
-  n->negative = false;
   struct biggie *n_copy = biggie_copy(n);
   struct biggie *m_copy = biggie_copy(m);
   // set signs to positive for calculation's convenience
+  n_copy->negative = false;
   m_copy->negative = false;
-  struct biggie *zero = biggie_create("0");
+  // sum stores and adds up every level of calculation
+  struct biggie *sum = biggie_create("0");
   for (int i = 0; i < m_len; ++i) {
     struct biggie *product = biggie_create("0");
     for (int count = m_copy->digits[i] - '0'; count > 0; --count) {
@@ -257,26 +273,36 @@ void biggie_mult(struct biggie *n, const struct biggie *m) {
                               (strlen(product->digits) + i + 1) *
                               sizeof(char));
     for (int j = 0; j < i; ++j) {
+      // "leading" zeros on the front
       product->digits[j] = '0';
     } 
     strcpy(product->digits + i, digits_copy);
-    biggie_add(zero, product);
+    biggie_add(sum, product);
     free(digits_copy);
     biggie_destroy(product);
   }
   biggie_destroy(n_copy);
   biggie_destroy(m_copy);
-  n->negative = NEGATIVE;
-  n->digits = realloc(n->digits, (strlen(zero->digits) + 1)
+  if (n->negative == m->negative) {
+    // positive if same sign
+    n->negative = false;
+  } else {
+    n->negative = true;
+  }
+  n->digits = realloc(n->digits, (strlen(sum->digits) + 1)
                       * sizeof(char));
-  strcpy(n->digits, zero->digits);
-  biggie_destroy(zero);
+  strcpy(n->digits, sum->digits);
+  biggie_destroy(sum);
+  n->digits = delete_leading_zero(n->digits);
   if (! strcmp(n->digits, "0")) {
     n->negative = false;
   }
 }
 
+
 bool biggie_eq(const struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
   if (n->negative != m->negative) {
     return false;
   }
@@ -291,6 +317,8 @@ bool biggie_eq(const struct biggie *n, const struct biggie *m) {
 }
 
 bool biggie_gt(const struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
   if (n->negative != m->negative) {
     if (n->negative) {
       return false;
@@ -301,20 +329,27 @@ bool biggie_gt(const struct biggie *n, const struct biggie *m) {
     struct biggie *copy = biggie_copy(n);
     biggie_sub(copy, m);
     // check if diff is positive and greater than zero
-    if ((! copy->negative) && 
-        ((strlen(copy->digits) > 1) || 
-         // either have multiple digits or the single digit is not zero
-         (copy->digits[0] != '0'))) {
+    if (copy->negative) {
+      // if (a - b) is negative, a < b
       biggie_destroy(copy);
-      return true;
+      return false;
     } else {
-      biggie_destroy(copy);
+      if ((strlen(copy->digits) == 1) && 
+          (copy->digits[0] == '0')) {
+        // single digit cannot be zero
+        biggie_destroy(copy);
+        return false;
+      } else {
+        biggie_destroy(copy);
+        return true;
+      }
       return false;
     }
   }
 }
 
 struct biggie *biggie_copy(const struct biggie *n) {
+  assert(n);
   struct biggie *new = malloc(sizeof(struct biggie));
   new->negative = n->negative;
   int len = strlen(n->digits);
@@ -324,4 +359,52 @@ struct biggie *biggie_copy(const struct biggie *n) {
 }
 
 void biggie_div(struct biggie *n, const struct biggie *m) {
+  assert(n);
+  assert(m);
+  struct biggie *zero = biggie_create("0");
+  // m cannot equal to zero
+  assert(! biggie_eq(m, zero));
+  biggie_destroy(zero);
+  int NEGATIVE = 0;
+  if (n->negative == m->negative) {
+    // positive if same sign
+    NEGATIVE = 0;
+  } else {
+    NEGATIVE = 1;
+  }
+  n->negative = false;
+  struct biggie *m_copy = biggie_copy(m);
+  m_copy->negative = false;
+  int current_digits = 1;
+  struct biggie *current = biggie_create("0");
+  for (int i = strlen(n->digits) - 1; i >= 0; --i) {
+    char digit = '0';
+    if (strcmp(current->digits, "0")) {
+      // if current is not "0", move all digits backward to 
+      //   create space for the new digit
+      char *digits_copy = malloc((current_digits + 1) 
+                                 * sizeof(char));
+      strcpy(digits_copy, current->digits);
+      ++current_digits;
+      current->digits = realloc(current->digits, 
+                                (current_digits + 1)* sizeof(char));
+      strcpy(current->digits + 1, digits_copy);
+      free(digits_copy);
+    }
+    current->digits[0] = n->digits[i];
+    while (compare(m_copy->digits, current->digits) == -1) {
+      // current >= m
+      biggie_sub(current, m_copy);
+      ++digit;
+    }
+    n->digits[i] = digit;
+    current_digits = strlen(current->digits);
+  }
+  biggie_destroy(current);
+  biggie_destroy(m_copy);
+  n->digits = delete_leading_zero(n->digits);
+  n->negative = NEGATIVE;
+  if (! strcmp(n->digits, "0")) {
+    n->negative = false;
+  }
 }
